10e4c35d0369dc9a19db7c8197012976ab7f9d17
Kyle-Neale <kyle.a.neale@gmail.com>
2018-08-09 08:21:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,27 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      pirate_hash = params[:pirate]
+      @new_pirate = Pirate.new(pirate_hash)
+      ship_hash = pirate_hash[:ships]
+      ship_hash.each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
   end
 end
